771d0766cd3781248fd5255f6863d5b25c26b4cb
sitangshu-tech <sitangshu2005@gmail.com>
2026-03-27 02:54:03 +0530

    fix: added suparbase db and connection with ssl

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,12 @@
 const { Pool } = require("pg");
+const { ssl } = require("pg/lib/defaults");
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
 module.exports = pool;
